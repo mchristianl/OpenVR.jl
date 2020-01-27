d81d96b4b90b40fad64e5701a9b3786daf30f423
Marcus Christian Lehmann <lehmann_ch@web.de>
2020-01-27 22:39:09 +0100

    removed BinaryProvider and performing the download manually

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,11 @@
 # https://julialang.github.io/Pkg.jl/v1/creating-packages/#Adding-a-build-step-to-the-package.-1
 
-cd(dirname(@__DIR__))
+using SHA
+
+cd(@__DIR__)
 
 # https://github.com/JuliaPackaging/BinaryProvider.jl
-using BinaryProvider
+# using BinaryProvider
 
 # https://github.com/JuliaPackaging/BinDeps.jl
 # using BinDeps
@@ -17,9 +19,10 @@ using BinaryProvider
 
 # import Compat.Sys
 
-
-with_samples = true
-build_samples = true
+with_samples   = false
+build_samples  = false
+force_download = false
+keep_download  = false
 
 # for examples, see
 #  https://github.com/JuliaInterop/CxxWrap.jl/blob/master/deps/build.jl
@@ -27,15 +30,15 @@ build_samples = true
 #  https://github.com/JuliaGraphics/Cairo.jl/blob/master/deps/build.jl
 #  https://github.com/jonathanBieler/SimpleDirectMediaLayer.jl/blob/master/deps/build.jl
 
-# os_dirname = "unknown"
-# os_so_extension = ""
-# Sys.islinux()   &&                        (os_dirname = "linux64"; os_so_extension=".so")
-# Sys.isapple()   &&                        (os_dirname = "osx32"  ; os_so_extension=".dylib")
-# Sys.iswindows() && Sys.ARCH == :x86_32 && (os_dirname = "win32"  ; os_so_extension=".dll")
-# Sys.iswindows() && Sys.ARCH == :x86_64 && (os_dirname = "win64"  ; os_so_extension=".dll")
-# os_julia_include_dir = joinpath(dirname(Sys.BINDIR),"include","julia")
+os_dirname = "unknown"
+os_so_extension = ""
+Sys.islinux()   &&                        (os_dirname = "linux64"; os_so_extension=".so")
+Sys.isapple()   &&                        (os_dirname = "osx32"  ; os_so_extension=".dylib")
+Sys.iswindows() && Sys.ARCH == :x86_32 && (os_dirname = "win32"  ; os_so_extension=".dll")
+Sys.iswindows() && Sys.ARCH == :x86_64 && (os_dirname = "win64"  ; os_so_extension=".dll")
+os_julia_include_dir = joinpath(dirname(Sys.BINDIR),"include","julia")
 
-if Sys.iswindows()
+if build_samples && Sys.iswindows()
   # detect Visual studio https://docs.microsoft.com/de-de/cpp/build/building-on-the-command-line?view=vs-2017#developer_command_file_locations
   found = []
   program_files_x86_dir = ENV["ProgramFiles(x86)"]
@@ -77,20 +80,56 @@ if Sys.iswindows()
   end
 end
 
-prefix = Prefix("./deps") # `/` also on windows?
-@assert ~isempty(prefix.path) ""
+# prefix = Prefix("./deps") # `/` also on windows?
+# @assert ~isempty(prefix.path) ""
 
 openvr_version = "1.9.16"
 openvr_dirname = "openvr-$openvr_version"
-openvr_dir = joinpath(prefix.path,openvr_dirname)
+openvr_dir = joinpath(@__DIR__,openvr_dirname)
 openvr_sample_dir = joinpath(openvr_dir,"samples")
 openvr_bin_dir = joinpath(openvr_dir,"lib",os_dirname)
 
-openvr_url  = "https://github.com/ValveSoftware/openvr/archive/v$openvr_version.tar.gz"
-openvr_tar_gz_sha256 = "8264e8dc20c926bc1828407a588b0755e15314f48a9b96bd1e550ac030174ecb"
+ispath(openvr_dir) && force_download && rm(openvr_dir; recursive=true, force=true)
 
-ispath(openvr_dir) && rm(openvr_dir; recursive=true, force=true)
-install(openvr_url, openvr_tar_gz_sha256; prefix=prefix, force=true)
+# install(openvr_url, openvr_tar_gz_sha256; prefix=prefix, force=true)
+if !ispath(openvr_dir)
+  if Sys.islinux() || Sys.isapple()
+    openvr_url  = "https://github.com/ValveSoftware/openvr/archive/v$openvr_version.tar.gz"
+    openvr_tar_gz_sha256 = "8264e8dc20c926bc1828407a588b0755e15314f48a9b96bd1e550ac030174ecb"
+    println("downloading $openvr_url")
+
+    openvr_local_tar_gz="openvr-v$openvr_version.tar.gz"
+    (!isfile(openvr_local_tar_gz)) && Base.download(openvr_url, openvr_local_tar_gz)
+
+    println("Checksum...")
+    openvr_sha2_256_file = bytes2hex(open(openvr_local_tar_gz) do f;  sha2_256(f); end)
+    @assert openvr_tar_gz_sha256 == openvr_sha2_256_file "Checksum mismatch! Expected $openvr_tar_gz_sha256, downloaded $openvr_sha2_256_file"
+
+    println("extracting...")
+    run(`tar xf $openvr_local_tar_gz`)
+    (!keep_download) && rm(openvr_local_tar_gz)
+  elseif Sys.iswindows()
+    openvr_url  = "https://github.com/ValveSoftware/openvr/archive/v$openvr_version.zip"
+    openvr_zip_sha256 = "e7063fae06220345beb43f8d7a634ebde2be48099e5348f3fc4b506db771770c"
+    println("downloading $openvr_url")
+
+    openvr_local_zip="openvr-v$openvr_version.zip"
+    (!isfile(openvr_local_zip)) && Base.download(openvr_url, openvr_local_zip)
+
+    println("Checksum...")
+    openvr_sha2_256_file = bytes2hex(open(openvr_local_zip) do f;  sha2_256(f); end)
+    @assert openvr_zip_sha256 == openvr_sha2_256_file "Checksum mismatch! Expected $openvr_zip_sha256, downloaded $openvr_sha2_256_file"
+
+    println("extracting...")
+    # try; using ZipFile; catch e; using Pkg; Pkg.add("ZipFile"); using ZipFile; end
+    sevenzip_path = joinpath(Sys.BINDIR, "7z.exe")
+    !isfile(sevenzip_path) && (sevenzip_path = joinpath(Sys.BINDIR, "../libexec/7z.exe"))
+    @assert isfile(sevenzip_path) "7zip not found in $sevenzip_path."
+    run(`$(sevenzip_path) x -y $openvr_local_zip`)
+
+    (!keep_download) && rm(openvr_local_zip)
+  end
+end
 
 for d in [d for d in readdir(openvr_dir) if d ∉ ["lib", "headers", "LICENSE", with_samples ? "samples" : "", "README.md"] ]
   println("removing " * joinpath(openvr_dir,d))
